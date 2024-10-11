6aaeb69495d91652d18c6ab139e6fc1b04b24f0b
Alex Nordin <alexnordin.dev@outlook.com>
2024-10-11 16:13:48 +0200

    Add project files

---
+++ BankIdDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankIdDemoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
